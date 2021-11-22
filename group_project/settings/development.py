1470fdb190b5b830a6ceb87edaa9f73e5aed0aa9
Ochuole <ochuolelizzie@gmail.com>
2021-11-22 12:44:55 +0100

    Preparation for Deployment

---
+++ group_project/settings/development.py
@@ -0,0 +1,21 @@
+from group_project.settings.common import *
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env.str("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env.bool("DEBUG", default=False)
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
